a4dc673c34865255b21ab5c2d6937c94b7386777
alice-odamtten <sakyibeaodamtten@gmail.com>
2024-01-18 23:57:30 +0000

    test

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- a SQL script that creates a table
+CREATE TABLE IF NOT EXISTS users(
+        id int AUTO_INCREMENT NOT NULL,
+        email varchar(255) NOT NULL UNIQUE,
+        name varchar(255),
+	country ENUM('US', 'CO', 'TN'),
+        PRIMARY KEY (id)
+        );
