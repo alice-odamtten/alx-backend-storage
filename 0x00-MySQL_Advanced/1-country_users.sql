6a8d6c5f2db3bb50d8c811dab015226b20f9a8d2
alice-odamtten <sakyibeaodamtten@gmail.com>
2024-01-19 00:00:01 +0000

    test

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS users(
         id int AUTO_INCREMENT NOT NULL,
         email varchar(255) NOT NULL UNIQUE,
         name varchar(255),
-	country ENUM('US', 'CO', 'TN'),
+	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
         PRIMARY KEY (id)
         );
